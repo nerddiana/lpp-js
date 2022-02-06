fdfa2e8c4354481c9b019e7652a602caa90147e3
Diana Martinez <diananerdoficial@gmail.com>
2022-02-05 22:24:23 -0600

    Define tokens

---
+++ src/modules/token.js
@@ -0,0 +1,27 @@
+class TokenType {
+    static ASSIGN = new TokenType("ASSIGN")
+    static COMMA = new TokenType("COMMA")
+    static EOF = new TokenType("EOF")
+    static FUNCTION = new TokenType("FUNCTION")
+    static IDENT = new TokenType("IDENT")
+    static ILEGAL = new TokenType("ILEGAL")
+    static INT = new TokenType("INT")
+    static LBRACE = new TokenType("LBRACE")
+    static LET = new TokenType("LET")
+    static LPAREN = new TokenType("LPAREN")
+    static PLUS = new TokenType("PLUS")
+    static RBRACE = new TokenType("RBRACE")
+    static RPAREN = new TokenType("RPAREN")
+    static SEMICOLON = new TokenType("SEMICOLON")
+
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Token {
+    constructor(tokenType, literal) {
+        this.tokenType = tokenType;
+        this.literal = literal;
+    }
+}
