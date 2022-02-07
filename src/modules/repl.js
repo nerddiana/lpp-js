dbc414f74cda308cb6ca1628e7d097ce8a40b6a5
Diana Martinez <diananerdoficial@gmail.com>
2022-02-06 19:33:40 -0600

    Add error logs to repl

--- src/modules/repl.js
+++ src/modules/repl.js
@@ -6,7 +6,7 @@ module.exports = {
         const lexer = new Lexer(uInput);
         const parser = new Parser(lexer);
         const program = parser.parseProgram();
-        console.dir(program, { depth: null });
+        console.log(parser.errors.join("\n"));
         callback();
     },
 };
