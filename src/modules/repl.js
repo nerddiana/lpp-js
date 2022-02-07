f227c9cb9cc895fc8bbc0075417caf4de4e83f1f
Diana Martinez <diananerdoficial@gmail.com>
2022-02-06 18:21:45 -0600

    Update repl for parse programs with LetStatements

--- src/modules/repl.js
+++ src/modules/repl.js
@@ -1,15 +1,13 @@
 const { Lexer } = require("./lexer.js");
+const { Parser } = require("./parser.js");
 const { TokenType } = require("./token.js");
 
 module.exports = {
     lexer(uInput, context, filename, callback) {
         const lexer = new Lexer(uInput);
-        let tokens = [];
-        while (true) {
-            const token = lexer.nextToken();
-            if (token.tokenType === TokenType.EOF) break;
-            tokens.push(token);
-        }
-        callback(tokens);
+        const parser = new Parser(lexer);
+        const program = parser.parseProgram();
+        console.dir(program, { depth: null });
+        callback();
     },
 };
